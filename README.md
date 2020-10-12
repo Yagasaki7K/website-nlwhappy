7ffc5a5638f8e694553f87b5fa15f81ac67befcc
Anderson "Yagasaki" Marlon <anderson18_marlon@hotmail.com>
2020-10-12 17:45:40 -0300

    commit initial

--- README.md
+++
@@ -1,2 +0,0 @@
-# nlw-happy
-A experience for Next Level Week v2 - A Happy Project
---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/global.css';
+import Routes from './routes';
+
+import './styles/global.css';
+
+function App() {
+  return (<Routes/>);
+}
+
+export default App;
